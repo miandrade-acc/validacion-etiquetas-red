fd4c0d7a8702f8778d7c67ef96662171cbca6af0
miandrade-acc <marcelo.andrade@uees.edu.ec>
2025-10-23 09:09:24 -0500

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+"""
+MPLS Vision Bot - Validación Automática de Etiquetas de Red
+
+Paquete principal del sistema desarrollado para validar etiquetas físicas mediante visión computacional y bots conversacionales.
+
+Autores:
+- María Augusta Flores
+- Marcelo Ismael Andrade
+
+Versión: 1.0.0
+Licencia: MIT
+"""
